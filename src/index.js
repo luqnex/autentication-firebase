c492da3c6fdfcb095c76ea92cfdd30089847be76
Lucas Costa <lucas.costa4002@gmail.com>
2021-10-25 13:56:39 -0300

    Instalação do dotenv

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 import { AuthProvider } from './provider/auth';
 import { Routes } from './Routes'
+
+require('dotenv').config()
 
 ReactDOM.render(
   <AuthProvider>
